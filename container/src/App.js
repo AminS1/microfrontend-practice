ba5babdc5c8d25902a541b90d9f5b7f02785fa33
Swamiprasad Amin <swamiprasad.amin@talentica.com>
2025-11-06 23:47:47 +0530

    dashboard integration finished

--- container/src/App.js
+++ container/src/App.js
@@ -1,8 +1,9 @@
 import React, { lazy, Suspense, useEffect, useState } from 'react';
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import Progress from './components/Progress';
+import { createBrowserHistory } from 'history';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'con',
@@ -12,12 +13,20 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
+const history = createBrowserHistory();
+
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if(isSignedIn){
+      history.push('/dashboard');
+    }
+  }, [isSignedIn])
+
   return (
     <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
           <Suspense fallback={<Progress />}>
@@ -25,12 +34,15 @@ const App = () => {
               <Route path='/auth'>
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              <Route path='/dashboard' component={DashboardLazy} />
+              <Route path='/dashboard'>
+                {!isSignedIn && <Redirect to='/' />}
+                <DashboardLazy />
+              </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
-      </BrowserRouter>
+      </Router>
     </StylesProvider>
   );
 }
